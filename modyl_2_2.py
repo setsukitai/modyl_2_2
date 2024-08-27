59dc20817989514859cc649972e0a9db6437333a
setsukitai <bondicom4ic@yandex.ru>
2024-08-27 18:16:40 +0300

    Add files via upload

---
+++ modyl_2_2.py
@@ -0,0 +1,6 @@
+first = int(input('1: '))
+second = int(input('2: '))
+third = int(input('3: '))
+if first == second == third:print(3)
+elif first == second or first == third or second == third:print(2)
+else:print(0)
